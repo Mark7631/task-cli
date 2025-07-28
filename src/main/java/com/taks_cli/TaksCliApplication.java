42ecb66e57459c428200e143dfcc244964fe0319
mark7631 <lmark.gm@gmail.com>
2025-07-28 15:08:52 +0300

    init commit

---
+++ src/main/java/com/taks_cli/TaksCliApplication.java
@@ -0,0 +1,13 @@
+package com.taks_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaksCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaksCliApplication.class, args);
+	}
+
+}
