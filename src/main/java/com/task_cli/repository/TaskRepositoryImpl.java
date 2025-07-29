281a7b737986e613fb7f0900f263c43a363e727d
mark7631 <lmark.gm@gmail.com>
2025-07-29 11:00:08 +0300

    fixed critical bug

--- src/main/java/com/task_cli/repository/TaskRepositoryImpl.java
+++ src/main/java/com/task_cli/repository/TaskRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.task_cli.enums.RepositoryResponse;
 import com.task_cli.records.Task;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
@@ -14,14 +15,23 @@ import java.util.UUID;
 @Slf4j
 @Repository
 public class TaskRepositoryImpl implements TaskRepository {
+    @Value("${repository}")
+    private String repoFilePath;
+
     @Override
     public RepositoryResponse addTask(String task) {
-        File file = new File("tasks.json");
         try {
+            File file = new File(repoFilePath);
             ObjectMapper objectMapper = new ObjectMapper();
-            HashMap<String, Task> map = objectMapper.readValue(file, HashMap.class);
-            if (map.get(task) != null) return RepositoryResponse.UNABLE_TASK;
-            objectMapper.writeValue(file, new Task(UUID.randomUUID(), task, "notDone"));
+            HashMap<String, Task> map;
+            if (file.length() != 0) {
+                map = objectMapper.readValue(file, HashMap.class);
+                if (map.get(task) != null) return RepositoryResponse.UNABLE_TASK;
+            } else {
+                map = new HashMap<>();
+            }
+            map.put(task, new Task(UUID.randomUUID(), task, "notDone"));
+            objectMapper.writeValue(file, map);
             return RepositoryResponse.OK;
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -32,11 +42,17 @@ public class TaskRepositoryImpl implements TaskRepository {
     @Override
     public RepositoryResponse addTask(String task, String status) {
         try {
-            File file = new File("tasks.json");
+            File file = new File(repoFilePath);
             ObjectMapper objectMapper = new ObjectMapper();
-            HashMap<String, Task> map = objectMapper.readValue(file, HashMap.class);
-            if (map.get(task) != null) return RepositoryResponse.UNABLE_TASK;
-            objectMapper.writeValue(file, new Task(UUID.randomUUID(), task, status));
+            HashMap<String, Task> map;
+            if (file.length() != 0) {
+                map = objectMapper.readValue(file, HashMap.class);
+                if (map.get(task) != null) return RepositoryResponse.UNABLE_TASK;
+            } else {
+                map = new HashMap<>();
+            }
+            map.put(task, new Task(UUID.randomUUID(), task, status));
+            objectMapper.writeValue(file, map);
             return RepositoryResponse.OK;
         } catch (IOException e) {
             log.error(e.getMessage());
